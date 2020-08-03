f7d3c7cdee340c59562dcd424d0003644a518c99
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-08-03 14:07:28 -0400

    added surface

---
+++ MainSurface.java
@@ -0,0 +1,10 @@
+package com.example.test;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class MainSurface extends GLSurfaceView {
+    public MainSurface(Context context) {
+        super(context);
+    }
+}
